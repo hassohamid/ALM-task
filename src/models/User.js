82ea9567f15871c6e8c7c75820f8405209683f60
hassohamid <hassohamid@hotmail.com>
2025-05-23 02:01:39 +0200

    Lade till validering och unika constraints i User-modellen

--- src/models/User.js
+++ src/models/User.js
@@ -10,10 +10,21 @@ const User = sequelize.define("User", {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  profilePicture: {
+    type: DataTypes.STRING,
+    validate: {
+      isUrl: true,
+    },
   },
 });
 
